3a65060c04376ea4b347b63cdcaff8ea16050030
wolfram77 <wolfram77@gmail.com>
2020-02-26 14:30:26 +0530

    scripts: adverb_exceptions added

---
+++ scripts/ADVERB_EXCEPTIONS.js
@@ -0,0 +1,10 @@
+const ADV_EXC = new Map([
+  ["best", "well"],
+  ["better", "well"],
+  ["deeper", "deeply"],
+  ["farther", "far"],
+  ["further", "far"],
+  ["harder", "hard"],
+  ["hardest", "hard"]
+]);
+module.exports = ADV_EXC;
