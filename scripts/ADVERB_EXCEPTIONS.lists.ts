9f18e84a3b00fcb3f057e5ff89bde5e8e8b678a3
Subhajit Sahu <wolfram77@gmail.com>
2023-01-06 00:06:15 +0530

    :bug: rename to TypeScript files

---
+++ scripts/ADVERB_EXCEPTIONS.lists.ts
@@ -0,0 +1,17 @@
+const ADV_EXC = [[
+  "best",
+  "better",
+  "deeper",
+  "farther",
+  "further",
+  "harder",
+  "hardest"], [
+  "well",
+  "well",
+  "deeply",
+  "far",
+  "far",
+  "hard",
+  "hard"
+]];
+module.exports = ADV_EXC;
